70cc731dd48e9aa3852c67f16cb25c3a14950b6d
LaNice Powell <lanicemp@aol.com>
2019-09-10 17:12:07 -0400

    schedule that needs to be connected to the client.

---
+++ lib/math_tutoring_cli_pj/schedule.rb
@@ -0,0 +1,20 @@
+class Schedule 
+
+attr_accessor 
+
+def initialize 
+@monday = []
+@tuesday = [] 
+@wednesday = []
+@thursday = []
+@friday = []
+@saturday = [] 
+@sunday = []
+end 
+
+def mo
+    schedule = {
+
+    }
+end 
+end 
