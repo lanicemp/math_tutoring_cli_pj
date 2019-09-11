7f21c5ab1de64dbdbb092be0b4093ef795341d09
LaNice Powell <lanicemp@aol.com>
2019-09-11 10:32:02 -0400

    testing out a new scraper method using json

--- lib/math_tutoring_cli_pj/scraper.rb
+++ lib/math_tutoring_cli_pj/scraper.rb
@@ -4,10 +4,16 @@ class MathTutoringCliPj::Scraper
 
 
     def self.scrape
-
         site = "https://www.kutasoftware.com"
         page = Nokogiri::HTML(open(site))
-        doc = page.css('div.section')
-        binding.pry 
-    end  
+        #doc = page.css(".section-group div")[1]
+        doc = page.css(".section-group div")[1].collect#{|icon| icon.attribute("href").value}
+        #binding.pry  
+    end
+      
+    def get_site 
+        string_data = open("https://www.kutasoftware.com").read
+        JSON.parse(string_data)
+    end 
+
 end 
