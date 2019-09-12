c3f2257b308ac97f6cde97eb5931136dfa9f77b8
LaNice Powell <lanicemp@aol.com>
2019-09-12 07:47:38 -0400

    added another scraper for the algebra 1 page

--- lib/math_tutoring_cli_pj/scraper.rb
+++ lib/math_tutoring_cli_pj/scraper.rb
@@ -1,19 +1,45 @@
+# require "pry"
+# require 'nokogiri'
+# require 'open-uri'
+# require 'irb'
+# require 'json'
+require_relative 'course'
 
-class MathTutoringCliPj::Scraper 
+module MathTutoringCliPj 
+    class Scraper 
 
+        def self.scrape
+            site = "https://www.kutasoftware.com"
+            page = Nokogiri::HTML(open(site))
+               page.css("div.section-group")[1].css("li")[0...-3]#.each do |li|
+                #  link = li.css("a").attr("href").value
+                #  name = li.css("a").text
+                #MathTutoringCliPj::Course.new(name,link)
+                #binding.pry 
+        end
 
+        def self.pre_algebra_scrape
+            site = "https://www.kutasoftware.com/freeipa.html"
+            page = Nokogiri::HTML(open(site))
+            page.css("div.topicbox")
+            # topics=   page.css(".TopicBox").css("span").text.split(" ")
+            # worksheet_links = page.css(".TopicBox").css("a").attr("href").value
+            # worksheet_names = page.css(".TopicBox").css("a").text
+            #binding.pry 
+        end 
 
-    def self.scrape
-        site = "https://www.kutasoftware.com"
-        page = Nokogiri::HTML(open(site))
-        #doc = page.css(".section-group div")[1]
-        doc = page.css(".section-group div")[1].collect#{|icon| icon.attribute("href").value}
-        #binding.pry  
-    end
-      
-    def get_site 
-        string_data = open("https://www.kutasoftware.com").read
-        JSON.parse(string_data)
+        def self.algebra1_scrape
+            site = "https://www.kutasoftware.com/free.html"
+            page = Nokogiri::HTML(open(site))
+            page.css("div.topicbox")
+            # topics=   page.css(".TopicBox").css("span").text.split(" ")
+            # worksheet_links = page.css(".TopicBox").css("a").attr("href").value
+            # worksheet_names = page.css(".TopicBox").css("a").text
+            binding.pry 
+        end    
+
+            
     end 
-
 end 
+#  MathTutoringCliPj::Scraper.second_scrape 
+# binding.pry
