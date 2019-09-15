5eabc2658273e34342a231afa91990a17a2af16f
LaNice Powell <lanicemp@aol.com>
2019-09-15 14:04:04 -0400

    removed notes

--- lib/require.rb
+++ lib/require.rb
@@ -9,12 +9,4 @@ require_relative "./math_tutoring_cli_pj/version"
 require_relative './math_tutoring_cli_pj/scraper'
 require_relative './math_tutoring_cli_pj/course'
 require_relative './math_tutoring_cli_pj/cli'
-require_relative './math_tutoring_cli_pj/client'
 
- #module MathTutoring
-  # class Error < StandardError; end
-  # Your code goes here...
- #end
-
-#Notes 
-# 1. the require files have to go on top of the require relative files so that they can be used in the different files. 
